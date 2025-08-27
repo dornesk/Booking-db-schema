b229547a1a073421b109c2d1402adb2b1ef36c1f
dornesk <dornesk@icloud.com>
2025-08-27 14:29:08 +0300

    Add basics of schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,60 @@
+CREATE TYPE booking_status AS ENUM ('pending', 'confirmed', 'canceled');
+
+CREATE TABLE users ( --пользователи: 1 пользователь - много бронирований (one-to-many)
+    user_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone VARCHAR(20),
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE properties ( --объекты для аренды: один пользователь - много объектов (one-to-many)
+    property_id SERIAL PRIMARY KEY,
+    owner_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    address TEXT NOT NULL,
+    price_per_night NUMERIC(10,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT fk_owner FOREIGN KEY (owner_id)
+        REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE bookings ( --бронирования: 1 пользователь - много бронирований; 1 объект - много бронирований (one-to-many)
+    booking_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    property_id INT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    status booking_status DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT fk_booking_user FOREIGN KEY (user_id)
+        REFERENCES users(user_id) ON DELETE CASCADE,
+    CONSTRAINT fk_booking_property FOREIGN KEY (property_id)
+        REFERENCES properties(property_id) ON DELETE CASCADE,
+    CONSTRAINT chk_dates CHECK (end_date >= start_date) -- проверка корректности дат
+);
+
+CREATE TABLE payments ( -- платежи: каждое бронирование имеет один платеж (one-to-one)
+    payment_id SERIAL PRIMARY KEY,
+    booking_id INT UNIQUE NOT NULL,        -- Один платеж на одно бронирование
+    amount NUMERIC(10,2) NOT NULL,
+    payment_date TIMESTAMP DEFAULT NOW(),
+    payment_method VARCHAR(20) NOT NULL,  -- Например: card, cash
+    CONSTRAINT fk_payment_booking FOREIGN KEY (booking_id)
+        REFERENCES bookings(booking_id) ON DELETE CASCADE
+);
+
+CREATE TABLE reviews ( --отзывы: -- many-to-many через таблицу отзывов: пользователь ↔ объект
+    review_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    property_id INT NOT NULL,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    comment TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT fk_review_user FOREIGN KEY (user_id)
+        REFERENCES users(user_id) ON DELETE CASCADE,
+    CONSTRAINT fk_review_property FOREIGN KEY (property_id)
+        REFERENCES properties(property_id) ON DELETE CASCADE,
+    CONSTRAINT uq_review UNIQUE(user_id, property_id) -- Один отзыв от пользователя на один объект
+);
